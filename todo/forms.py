2a918d24d039190e01b7f3fe8799947d96e9f716
vinayak <vinayak>
2019-02-18 20:13:34 +0530

    First commit

---
+++ todo/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Todo
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields="__all__"
---
+++ todo/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+
+## import todo form and models
+
+from .forms import TodoForm
+from .models import Todo
+
+###############################################
+
+def index(request):
+
+    item_list = Todo.objects.order_by("-date")
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo')
+    form = TodoForm()
+    page = {
+             "forms" : form,
+             "list" : item_list,
+             "title" : "TODO LIST",
+           }
+    return render(request,'todo/index.html',page)
+
+
+### function to remove item , it recive todo item id from url ##
+def remove(request, item_id):
+    item = Todo.objects.get(id=item_id)
+    item.delete()
+    messages.info(request,"item removed !!!")
+    return redirect('todo')
